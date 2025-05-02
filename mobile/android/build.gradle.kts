695b67377d550d8838e438ddac8b6240dd21d946
rockan007 <rockan007@gmail.com>
2025-05-02 21:57:54 +0800

    [bugfix]namespace not specialed

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -12,6 +12,24 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// Fix for namespace issues in plugin modules
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.findByName("android")
+            if (android != null && android.javaClass.methods.any { it.name == "getNamespace" }) {
+                val getNamespace = android.javaClass.getMethod("getNamespace")
+                val namespace = getNamespace.invoke(android) as String?
+                if (namespace == null) {
+                    val setNamespace = android.javaClass.getMethod("setNamespace", String::class.java)
+                    setNamespace.invoke(android, project.group.toString())
+                }
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
